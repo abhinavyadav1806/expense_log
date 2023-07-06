a2dab35f4b639c1f6f82a5098ae4ba22c2e7b6a0
abhinavyadav1806 <abhinav1806yadav@gmail.com>
2023-07-06 23:44:56 +0530

    Lecture 56: Arrow Function

--- src/App.js
+++ src/App.js
@@ -1,48 +1,58 @@
-// import logo from './logo.svg';
-// import './App.css';
-import ExpenseItem from "./components/ExpenseItem";
+import React from 'react';
 
-function App() {
+import Expenses from './components/Expenses/Expenses';
+
+// function App () {
+const App = () => {
   const expenses = [
     {
-      id: "e1",
-      title: "Toilet Paper",
+      id: 'e1',
+      title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(2023, 7, 14),
+      date: new Date(2020, 7, 14),
     },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2023, 2, 12),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
+      id: 'e3',
+      title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(2023, 2, 28),
+      date: new Date(2021, 2, 28),
     },
     {
-      id: "e4",
-      title: "New Desk (Wooden)",
+      id: 'e4',
+      title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date(2023, 6, 3),
+      date: new Date(2021, 5, 12),
     },
   ];
 
-  return (
-    <div className="App">
-      <body>
-        <h1 class="heading_wrapper">Expense Data</h1>
-        <ExpenseItem  title="Toilet Paper" amount="94.12" date={new Date(2023, 5, 5)} />
+  // return (
+  //   <div className="App">
+  //     <body>
+  //       <h1 class="heading_wrapper">Expense Data</h1>
+  //       <ExpenseItem  title="Toilet Paper" amount="94.12" date={new Date(2023, 5, 5)} />
 
-        {/* -- This is sending complete array object as a single parameter. -- */}
-        {/* <ExpenseItem  expense={expenses[1]} /> */}
+  //       {/* -- This is sending complete array object as a single parameter. -- */}
+  //       {/* <ExpenseItem  expense={expenses[1]} /> */}
         
-        <ExpenseItem  title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-        <ExpenseItem  title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-        <ExpenseItem  title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
-      </body>
+  //       <ExpenseItem  title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+  //       <ExpenseItem  title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+  //       <ExpenseItem  title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+  //     </body>
+  //   </div>
+  // );
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+/* -- This is called object destructuring. -- */
+// export default function ExpenseItem({date, title, amount})
+const ExpenseItem = (props) => {
+  return (
+    // This is sending complete array object as a single parameter.
+    // <div>{props.expense.date.toDateString()}</div>
+    //   <div className="expense-item__description">
+    //     <h2>{props.expense.title}</h2>
+    //     <div className="expense-item__price">${props.expense.amount}</div>
+    // </div>
+
+    // This is called object destructuring.
+    // <div>{date.toDateString()}</div>
+    //   <div className="expense-item__description">
+    //     <h2>{title}</h2>
+    //     <div className="expense-item__price">${amount}</div>
+    // </div>
+
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
